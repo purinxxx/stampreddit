0f707f9974e0b90af7cb4403e37e15e8bccceebb
purinxxx <kengo@MacBook-Pro.local>
2015-08-28 15:15:14 +0900

    疲れた

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ $url = $_GET["url"];
 $css = file_get_contents($url);
 preg_match_all('/a\[href.*?="#(.*?)"]/',$css,$out);
 
-foreach ($out[1] as $key => $value){
+foreach (array_unique($out[1]) as $key => $value){
   echo '<a href="#'.$value.'"></a>';
 }
